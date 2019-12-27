b9b234c0cf58820b2c4fa8dfc152b975c3388426
romelth <romel.20120309@unas.edu.pe>
2019-12-27 09:00:24 -0500

    trabajo con seeder y factory

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use PhpParser\Node\Stmt\Foreach_;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+
+         $this->truncateTablas([
+             'rol',
+             'permiso'
+         ]);
+        $this->call(TablaRolSeeder::class);
+        $this->call(TablaPermisoSeeder::class);
+    }
+    protected function truncateTablas(array $tablas){
+       DB::statement('SET FOREIGN_KEY_CHECKS = 0;');
+       foreach($tablas as $tabla){
+          DB::table($tabla)->truncate();
+       }
+       DB::statement('SET FOREIGN_KEY_CHECKS=1');
+
     }
 }
